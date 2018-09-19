8e9fe29d604cfe739d6be6cdcef5c7f70bb97e9c
gcarde <43408921+gcarde@users.noreply.github.com>
2018-09-19 09:31:56 -0300

    Code_1_evolved.R

--- Code_1.R
+++ Code_1.R
@@ -1,3 +1,4 @@
 f <- function(x) {sin(x)*cos(x);} ##setting the function to (x)*cos(x)sin
 plot(f,-5*p,5*pi) ##this allows the graph to be plotted between -5 of pi and +5 of pi
+abline(0, 1) ## adds a straight line
 
